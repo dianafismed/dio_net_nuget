b0f06e7f4fd5a7679e514b4d3ef952ca7592b0c4
Diana Avila <missernightangel@gmail.com>
2025-09-03 17:24:35 -0300

    up

--- Program.cs
+++ Program.cs
@@ -3,17 +3,16 @@ using Newtonsoft.Json;
 
 DateTime dataAtual = DateTime.Now;
 
+// // Serializando
 // List<Venda> listaVendas = new List<Venda>();
-
 // Venda venda1 = new Venda(1, "Notebook", 3500.00m, dataAtual);
 // listaVendas.Add(venda1);
-
 // Venda venda2 = new Venda(2, "Smartphone", 2500.00m, dataAtual);
 // listaVendas.Add(venda2);
-
 // string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented); // não é obrigatório o Formatting.Indented
 // File.WriteAllText("Arquivos/venda.json", serializado);
 
+// Desserializando
 string conteudoArquivo = File.ReadAllText("Arquivos/livro.json");
 List<Livro> listaLivros = JsonConvert.DeserializeObject<List<Livro>>(conteudoArquivo);
 foreach (var livro in listaLivros)
