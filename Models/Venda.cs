88b9bb6379fa2d2e32255cc50e0e0353e22d0606
Diana Avila <missernightangel@gmail.com>
2025-09-03 17:16:16 -0300

    datetime

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -7,11 +7,12 @@ namespace dio_net_nuget.Models
 {
     public class Venda
     {
-        public Venda(int id, string produto, decimal preco)
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
         {
             Id = id;
             Produto = produto;
             Preco = preco;
+            DataVenda = dataVenda;
         }
 
         public int Id { get; set; }
@@ -19,5 +20,7 @@ namespace dio_net_nuget.Models
         public string Produto { get; set; }
 
         public decimal Preco { get; set; }
+
+        public DateTime DataVenda { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿using dio_net_nuget.Models;
 using Newtonsoft.Json;
 
+DateTime dataAtual = DateTime.Now;
+
 List<Venda> listaVendas = new List<Venda>();
 
-Venda venda1 = new Venda(1, "Notebook", 3500.00m);
+Venda venda1 = new Venda(1, "Notebook", 3500.00m, dataAtual);
 listaVendas.Add(venda1);
 
-Venda venda2 = new Venda(2, "Smartphone", 2500.00m);
+Venda venda2 = new Venda(2, "Smartphone", 2500.00m, dataAtual);
 listaVendas.Add(venda2);
 
-string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented); // não é obrigatório o Formatting.Indented
 File.WriteAllText("Arquivos/venda.json", serializado);
