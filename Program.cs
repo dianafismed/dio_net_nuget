be9346f8bae3495bd98b4163edaf0d088b3232f9
Diana Avila <missernightangel@gmail.com>
2025-09-03 17:09:44 -0300

    gerando arquivo json

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using dio_net_nuget.Models;
 using Newtonsoft.Json;
 
+List<Venda> listaVendas = new List<Venda>();
+
 Venda venda1 = new Venda(1, "Notebook", 3500.00m);
+listaVendas.Add(venda1);
 
-string serializado = JsonConvert.SerializeObject(venda1, Formatting.Indented);
+Venda venda2 = new Venda(2, "Smartphone", 2500.00m);
+listaVendas.Add(venda2);
 
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 File.WriteAllText("Arquivos/venda.json", serializado);
-
-//Console.WriteLine(serializado);
